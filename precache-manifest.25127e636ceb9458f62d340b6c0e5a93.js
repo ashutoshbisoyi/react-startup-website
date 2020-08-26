10809a1bdd1326ee2e2010c2989d309e840a9652
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2020-08-26 18:11:34 +0530

    Updates

---
+++ precache-manifest.25127e636ceb9458f62d340b6c0e5a93.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa9f04bd715a6eb97f9438e8622406c",
+    "url": "/react-startup-website/index.html"
+  },
+  {
+    "revision": "a0b81a1db91b2383b594",
+    "url": "/react-startup-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a41c52857aca69b16d13",
+    "url": "/react-startup-website/static/css/main.c29a6cdd.chunk.css"
+  },
+  {
+    "revision": "a0b81a1db91b2383b594",
+    "url": "/react-startup-website/static/js/2.6de24942.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/react-startup-website/static/js/2.6de24942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41c52857aca69b16d13",
+    "url": "/react-startup-website/static/js/main.dcce29dc.chunk.js"
+  },
+  {
+    "revision": "f8cc079d2630f6c2fb1d",
+    "url": "/react-startup-website/static/js/runtime-main.971de796.js"
+  },
+  {
+    "revision": "592ad7e7e8333f3f0c8a3296b18c02a1",
+    "url": "/react-startup-website/static/media/aboutimg.592ad7e7.jpg"
+  },
+  {
+    "revision": "8b07339dda384ba54043f2941a39786f",
+    "url": "/react-startup-website/static/media/admaking.8b07339d.jpg"
+  },
+  {
+    "revision": "04507997ac26780b330e1cdede7d4504",
+    "url": "/react-startup-website/static/media/bannermaking.04507997.jpg"
+  },
+  {
+    "revision": "579ae7627b70a9f32ef324d21e741ad4",
+    "url": "/react-startup-website/static/media/businessconsulting.579ae762.jpg"
+  },
+  {
+    "revision": "d8e8f7946f2345319c353df76b449756",
+    "url": "/react-startup-website/static/media/clientimg.d8e8f794.webp"
+  },
+  {
+    "revision": "8c9ccecd6de963b7ad2347b126d2ae6f",
+    "url": "/react-startup-website/static/media/customprinting.8c9ccecd.jpg"
+  },
+  {
+    "revision": "ea1933c64424ccfa5bbfdd90e8cbe645",
+    "url": "/react-startup-website/static/media/eventcapturing.ea1933c6.jpg"
+  },
+  {
+    "revision": "02096c0c1a3b47dca92efd70524da370",
+    "url": "/react-startup-website/static/media/graphicsdesgining.02096c0c.jpg"
+  },
+  {
+    "revision": "2c9710057b15f386cf37d3d31b4f5d83",
+    "url": "/react-startup-website/static/media/haneulpublication.2c971005.png"
+  },
+  {
+    "revision": "6aba15417ebe20bfae60fe9b66e57723",
+    "url": "/react-startup-website/static/media/honourcreatorsimg.6aba1541.png"
+  },
+  {
+    "revision": "e47ab2f80916d088e609b4861fdc0ff9",
+    "url": "/react-startup-website/static/media/inexttechnology.e47ab2f8.png"
+  },
+  {
+    "revision": "6c8792b74a949ff6f6514c5b69d08cb9",
+    "url": "/react-startup-website/static/media/logodesgining.6c8792b7.jpg"
+  },
+  {
+    "revision": "ad389e049ce92016f1b66a4af07b321f",
+    "url": "/react-startup-website/static/media/ninthsem.ad389e04.png"
+  },
+  {
+    "revision": "94e38dc03277e9a8ee9e847f2a439625",
+    "url": "/react-startup-website/static/media/ourvoix.94e38dc0.png"
+  },
+  {
+    "revision": "4c12008a0d5f7c4d66c81cb7342056bb",
+    "url": "/react-startup-website/static/media/rajeshvision.4c12008a.png"
+  },
+  {
+    "revision": "6688aa49eb72d60f5cddb07fbac8eaa0",
+    "url": "/react-startup-website/static/media/voyagers.6688aa49.png"
+  },
+  {
+    "revision": "ef53e2b265749a3e28d1b111c60a5bd0",
+    "url": "/react-startup-website/static/media/whyimg.ef53e2b2.jpg"
+  }
+]);
