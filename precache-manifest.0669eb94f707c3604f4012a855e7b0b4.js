0d87a6f397cae41357bd8e60d096f92fd243a42d
ashutoshbisoyi <ashutoshbisoyi205@gmail.com>
2021-01-03 18:03:05 +0530

    Updates

---
+++ precache-manifest.0669eb94f707c3604f4012a855e7b0b4.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a2296e58f609eb39e22f598ed33a50",
+    "url": "/react-startup-website/index.html"
+  },
+  {
+    "revision": "26f722fa789a04c35dab",
+    "url": "/react-startup-website/static/css/2.0b989fd1.chunk.css"
+  },
+  {
+    "revision": "f57f9ac3af2ea3124ce3",
+    "url": "/react-startup-website/static/css/main.567978fe.chunk.css"
+  },
+  {
+    "revision": "26f722fa789a04c35dab",
+    "url": "/react-startup-website/static/js/2.b80dc9b2.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/react-startup-website/static/js/2.b80dc9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57f9ac3af2ea3124ce3",
+    "url": "/react-startup-website/static/js/main.da721d82.chunk.js"
+  },
+  {
+    "revision": "f8cc079d2630f6c2fb1d",
+    "url": "/react-startup-website/static/js/runtime-main.971de796.js"
+  },
+  {
+    "revision": "571009f12e4f25c1040d325e1625696a",
+    "url": "/react-startup-website/static/media/aboutimg.571009f1.jpg"
+  },
+  {
+    "revision": "33d57daf56770a593fac4940927e4764",
+    "url": "/react-startup-website/static/media/admaking.33d57daf.jpg"
+  },
+  {
+    "revision": "17e0b9feda9499f789bd4e00da2839d5",
+    "url": "/react-startup-website/static/media/app.17e0b9fe.jpg"
+  },
+  {
+    "revision": "3f9d3997018163dab9ddd4fbdfc6c2dd",
+    "url": "/react-startup-website/static/media/app1.3f9d3997.jpg"
+  },
+  {
+    "revision": "895cf8364cba7d835547261ef716480d",
+    "url": "/react-startup-website/static/media/app2.895cf836.jpg"
+  },
+  {
+    "revision": "72c184b717a749b2be5a69b23a7e36fd",
+    "url": "/react-startup-website/static/media/app3.72c184b7.jpg"
+  },
+  {
+    "revision": "de132e7c35faaf42e13c38fbf03f6b56",
+    "url": "/react-startup-website/static/media/app4.de132e7c.jpg"
+  },
+  {
+    "revision": "970cc80a2c766d657a1ea3a945fd28ec",
+    "url": "/react-startup-website/static/media/app5.970cc80a.jpg"
+  },
+  {
+    "revision": "d1aeafd26c747b1346401771dbfcd360",
+    "url": "/react-startup-website/static/media/app6.d1aeafd2.jpg"
+  },
+  {
+    "revision": "db1a9a870e46a88d5037b254db8afb59",
+    "url": "/react-startup-website/static/media/bannermaking.db1a9a87.jpg"
+  },
+  {
+    "revision": "69cdea837e7a3b0e74237149ee833d06",
+    "url": "/react-startup-website/static/media/businessconsulting.69cdea83.jpg"
+  },
+  {
+    "revision": "d8e8f7946f2345319c353df76b449756",
+    "url": "/react-startup-website/static/media/clientimg.d8e8f794.webp"
+  },
+  {
+    "revision": "8baefbc652b2816d563af6670360504a",
+    "url": "/react-startup-website/static/media/customprinting.8baefbc6.jpg"
+  },
+  {
+    "revision": "9ccdae913b1b9958838f13994baf717c",
+    "url": "/react-startup-website/static/media/digitalmarketing-banner.9ccdae91.jpg"
+  },
+  {
+    "revision": "9b324c8f0059ff09a2f14bbeb8c40b11",
+    "url": "/react-startup-website/static/media/dm1.9b324c8f.jpg"
+  },
+  {
+    "revision": "7239957cd7ea71fe5267983592b30d9e",
+    "url": "/react-startup-website/static/media/dm2.7239957c.jpg"
+  },
+  {
+    "revision": "3b72ef403f360614c1e1c8a36ee0555b",
+    "url": "/react-startup-website/static/media/dm3.3b72ef40.jpg"
+  },
+  {
+    "revision": "b72854441beb8b90917c1c84bc885461",
+    "url": "/react-startup-website/static/media/dm4.b7285444.jpg"
+  },
+  {
+    "revision": "43c76945b9738865d6a88fe64f7f8483",
+    "url": "/react-startup-website/static/media/dm5.43c76945.jpg"
+  },
+  {
+    "revision": "f05f6db12ef3c81279afb0f0184aecda",
+    "url": "/react-startup-website/static/media/dm6.f05f6db1.jpg"
+  },
+  {
+    "revision": "ecd662c702bdf61baa72d9dd9b3bf253",
+    "url": "/react-startup-website/static/media/dm7.ecd662c7.jpg"
+  },
+  {
+    "revision": "69d63a5a5aed0c4d2e0ec406c947f249",
+    "url": "/react-startup-website/static/media/eventcapturing.69d63a5a.jpg"
+  },
+  {
+    "revision": "28f34b2d311879c951a75904cfc95c8e",
+    "url": "/react-startup-website/static/media/graphicsdesgining.28f34b2d.jpg"
+  },
+  {
+    "revision": "2c9710057b15f386cf37d3d31b4f5d83",
+    "url": "/react-startup-website/static/media/haneulpublication.2c971005.png"
+  },
+  {
+    "revision": "6b71c7567d54573a52bcf70fdd633cca",
+    "url": "/react-startup-website/static/media/honourcreators.6b71c756.png"
+  },
+  {
+    "revision": "6b71c7567d54573a52bcf70fdd633cca",
+    "url": "/react-startup-website/static/media/honourcreatorsimg.6b71c756.png"
+  },
+  {
+    "revision": "e47ab2f80916d088e609b4861fdc0ff9",
+    "url": "/react-startup-website/static/media/inexttechnology.e47ab2f8.png"
+  },
+  {
+    "revision": "d0254fbd9661f4104ba50b3c024631c0",
+    "url": "/react-startup-website/static/media/ld1.d0254fbd.jpg"
+  },
+  {
+    "revision": "5be4d28f7d5b3dd84189d26631bad885",
+    "url": "/react-startup-website/static/media/ld2.5be4d28f.jpg"
+  },
+  {
+    "revision": "eea7b75c1967826024f215194364046a",
+    "url": "/react-startup-website/static/media/ld3.eea7b75c.jpg"
+  },
+  {
+    "revision": "fa122fa90d3a0ed1cfca9e633b88c290",
+    "url": "/react-startup-website/static/media/ld4.fa122fa9.jpg"
+  },
+  {
+    "revision": "e6408086600ab673c0280584b74ea597",
+    "url": "/react-startup-website/static/media/ld5.e6408086.jpg"
+  },
+  {
+    "revision": "2f612030fb5ce38497af933c3e5bd0fb",
+    "url": "/react-startup-website/static/media/ld6.2f612030.jpg"
+  },
+  {
+    "revision": "5d6baf4824f38c7a14d140eef0d112ff",
+    "url": "/react-startup-website/static/media/logo.5d6baf48.jpg"
+  },
+  {
+    "revision": "ad389e049ce92016f1b66a4af07b321f",
+    "url": "/react-startup-website/static/media/ninthsem.ad389e04.png"
+  },
+  {
+    "revision": "032e6e751596de587421f0040d7dcd3a",
+    "url": "/react-startup-website/static/media/noimg.032e6e75.jpg"
+  },
+  {
+    "revision": "94e38dc03277e9a8ee9e847f2a439625",
+    "url": "/react-startup-website/static/media/ourvoix.94e38dc0.png"
+  },
+  {
+    "revision": "4c12008a0d5f7c4d66c81cb7342056bb",
+    "url": "/react-startup-website/static/media/rajeshvision.4c12008a.png"
+  },
+  {
+    "revision": "6688aa49eb72d60f5cddb07fbac8eaa0",
+    "url": "/react-startup-website/static/media/voyagers.6688aa49.png"
+  },
+  {
+    "revision": "d94fb71e3010672cbd66ae4c8332f56d",
+    "url": "/react-startup-website/static/media/wd1.d94fb71e.jpg"
+  },
+  {
+    "revision": "11cdc4c9560502e11e5cfa63cc9e26b5",
+    "url": "/react-startup-website/static/media/wd2.11cdc4c9.jpg"
+  },
+  {
+    "revision": "c8cc20e354c55d252e6206df8ec1a357",
+    "url": "/react-startup-website/static/media/wd3.c8cc20e3.png"
+  },
+  {
+    "revision": "12461b95449234ce4dffb1b89ff52aae",
+    "url": "/react-startup-website/static/media/wd4.12461b95.png"
+  },
+  {
+    "revision": "ed5c88d7d58ea039eae9d232b01df793",
+    "url": "/react-startup-website/static/media/wd5.ed5c88d7.png"
+  },
+  {
+    "revision": "701104d0ff0fb927efd9df676443ca4b",
+    "url": "/react-startup-website/static/media/wd6.701104d0.jpg"
+  },
+  {
+    "revision": "b8b3acfa09defa5834f4f4a2c779e770",
+    "url": "/react-startup-website/static/media/webdev.b8b3acfa.jpg"
+  },
+  {
+    "revision": "38232a2248e2d4830bd3ce18be842df2",
+    "url": "/react-startup-website/static/media/whyimg.38232a22.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/honour/precache-manifest.d33633cb6c9b3221e65f113393928299.js"
+  "/react-startup-website/precache-manifest.0669eb94f707c3604f4012a855e7b0b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/honour/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-startup-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
